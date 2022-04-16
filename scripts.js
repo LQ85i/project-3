6c81695a56d2cd3dc7e4b3e7194bd3f86d8f567b
Funxy <jaakko.kakko97@gmail.com>
2022-04-16 21:16:32 +0300

    Add grid with a hover effect. Add non-functional menu buttons.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 function createGrid() {
+    const container = document.querySelector('.container');
+    for(let i = 0; i < 16; i++){
+        const row = document.createElement('div');
+        container.append(row);
+        row.className = 'row';
+        for(let j = 0; j< 16; j++){
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            row.append(cell);
+        }
+    }
+}
+
+createGrid();
+
+function setSize(){
+
+}
+
+function clearGrid() {
     
 }
