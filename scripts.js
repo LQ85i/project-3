c460b1150699c7eca59c80d51a4f7cd44836a100
Funxy <jaakko.kakko97@gmail.com>
2022-04-16 21:44:43 +0300

    Add functionality to grid size button.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-function createGrid() {
+function createGrid(size) {
     const container = document.querySelector('.container');
-    for(let i = 0; i < 16; i++){
+    for(let i = 0; i < size; i++){
         const row = document.createElement('div');
         container.append(row);
         row.className = 'row';
-        for(let j = 0; j< 16; j++){
+        for(let j = 0; j< size; j++){
             const cell = document.createElement('div');
             cell.className = 'cell';
             row.append(cell);
@@ -12,12 +12,30 @@ function createGrid() {
     }
 }
 
-createGrid();
+function removeGrid() {
+    const cells = document.querySelectorAll('.cell');
+    const rows = document.querySelectorAll('.row');
+    cells.forEach(e => e.remove());
+    rows.forEach(e => e.remove());
+}
+
+createGrid(16);
 
 function setSize(){
-
+    let size = prompt("Type in grid size (1-100): ");
+    if (isNaN(size)) {size = 16;}
+    if (size>100) {size = 100;}
+    if (size<1) {size = 1;}
+    removeGrid();
+    createGrid(size);
 }
 
 function clearGrid() {
-    
+
 }
+
+const sizeButton = document.querySelector('.menu .size');
+const resetButton = document.querySelector('.menu .reset');
+
+sizeButton.addEventListener('click', () => setSize());
+resetButton.addEventListener('click',() => clearGrid());
